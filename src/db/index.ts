c28883591b9f44cb43d0eeecee5764b310da7534
uriah-dev <otch.dev@gmail.com>
2026-01-09 15:05:22 +0000

    db cleint get optimization

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,7 +8,7 @@ let clientInstance: ReturnType<typeof postgres> | null = null;
 
 const getDB = () => {
   if (!env.DATABASE_URL) {
-    throw Error("DATABASE_URL is not configured")
+    throw Error("DATABASE_URL is not configured");
   }
   if (!dbInstance) {
     if (!clientInstance) {
@@ -19,6 +19,10 @@ const getDB = () => {
   return dbInstance;
 };
 
-export const db = getDB();
+export const db = new Proxy({} as ReturnType<typeof getDB>, {
+  get(_, prop) {
+    return (getDB() as any)[prop];
+  }
+});
 
-export type DBType = typeof db;
+export type DBType = ReturnType<typeof getDB>;
