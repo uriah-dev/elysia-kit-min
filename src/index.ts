c6d7c9ea775c942ce3da8686acf945d581938f30
uriah-dev <otch.dev@gmail.com>
2026-01-28 10:01:46 +0000

    refactor: Clean up imports and modify server listen configuration in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { server } from "@app/_app";
 import { env } from "@src/env";
-import { buildServiceUrl, hasValue, logger, trySyncWrapper, tryWrapper } from "@lib/utils";
+import {
+  buildServiceUrl,
+  hasValue,
+  logger,
+  trySyncWrapper,
+  tryWrapper,
+} from "@lib/utils";
 
 export type Server = typeof server;
 
 const PORT = process.env.PORT || env.APP_PORT!;
 
 const result = trySyncWrapper(() => {
-  server.listen({port: Number(PORT)});
+  server.listen({});
   logger.info(`🦊 Server is running at ${buildServiceUrl(PORT)}`);
   return { success: true };
 });
